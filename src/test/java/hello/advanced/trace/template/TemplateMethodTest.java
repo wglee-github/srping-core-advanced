e9b1dab162a3d93aec6d4b13b85e017ffdf3b064
wglee <amd0223@gmail.com>
2023-08-12 20:51:16 +0900

    섹션 3. 템플릿 메서드 패턴과 콜백 패턴
    6.전략 패턴 - 시작
    7.전략 패턴 - 예제1
    8.전략 패턴 - 예제2

--- src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
+++ src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
@@ -18,9 +18,9 @@ public class TemplateMethodTest {
 	
 	private void logic1() {
 		long startTime = System.currentTimeMillis();
-		
+		// 비즈니스 로직 실행
 		log.info("비즈니스 로직1 실행");
-		
+		// 비즈니스 로직 종료
 		long endTime = System.currentTimeMillis();
 		long resultTime = endTime - startTime;
 		
@@ -29,9 +29,9 @@ public class TemplateMethodTest {
 	
 	private void logic2() {
 		long startTime = System.currentTimeMillis();
-		
+		// 비즈니스 로직 실행
 		log.info("비즈니스 로직2 실행");
-		
+		// 비즈니스 로직 종료
 		long endTime = System.currentTimeMillis();
 		long resultTime = endTime - startTime;
 		
